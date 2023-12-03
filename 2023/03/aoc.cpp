382d98f925859e56c07d98038e8726d48b0c6661
s4mpl <brandanjroachell@att.net>
2023-12-03 04:37:41 -0500

    finished day 3

--- 2023/03/aoc.cpp
+++ 2023/03/aoc.cpp
@@ -30,6 +30,7 @@ void part1(ifstream &input) {
       k = schema[i][j];
       num = "";
       is_part = false;
+      
       if(k == '.') continue;
       if(isdigit(k)) {
         while(isdigit(schema[i][j]) && j < schema[0].size()) {
@@ -75,6 +76,8 @@ void part2(ifstream &input) {
   vector<string> schema;
   char k, around;
   bool is_part;
+  unordered_map<int, vector<int>> adj;
+  vector<int> cur_parts;
 
   while(getline(input, line)) {
     schema.push_back(line);
@@ -83,6 +86,9 @@ void part2(ifstream &input) {
   for(i = 0; i < schema.size(); i++) {
     for(j = 0; j < schema[0].size(); j++) {
       cout << schema[i][j];
+      if(!isdigit(schema[i][j]) && schema[i][j] != '.') {
+        adj.insert(make_pair((i*schema[0].size()+j), 0));
+      }
     }
     cout << endl;
   }
@@ -93,17 +99,19 @@ void part2(ifstream &input) {
       k = schema[i][j];
       num = "";
       is_part = false;
+      cur_parts.clear();
+
       if(k == '.') continue;
       if(isdigit(k)) {
         while(isdigit(schema[i][j]) && j < schema[0].size()) {
           num += schema[i][j];
           // check the 3 cells vertically to the left of the current number
-          // skip check if already a confirmed part
-          for(n = -1; n <= 1 && !is_part; n++) {
+          for(n = -1; n <= 1; n++) {
             if(i+n < 0 || i+n >= schema.size() || j-1 < 0) continue;
             around = schema[i+n][j-1];
             if(!isdigit(around) && around != '.') {
               is_part = true;
+              cur_parts.push_back((i+n)*schema[0].size()+(j-1));
             }
           }
           j++;
@@ -112,20 +120,35 @@ void part2(ifstream &input) {
         if(!is_part) {
           j--;
           for(m = 0; m <= 1; m++) {
-            for(n = -1; n <= 1 && !is_part; n++) {
+            for(n = -1; n <= 1; n++) {
               if(i+n < 0 || i+n >= schema.size() || j+m >= schema[0].size()) continue;
               around = schema[i+n][j+m];
               if(!isdigit(around) && around != '.') {
                 is_part = true;
+                cur_parts.push_back((i+n)*schema[0].size()+(j+m));
               }
             }
           }
         }
       }
-      
+
       if(is_part) {
-        res += stoi(num);
+        cout << num << " adj to " << cur_parts.size() << " symbol(s):";
+        for(auto part : cur_parts) {
+          cout << " " << part;
+          adj[part].push_back(stoi(num));
+        }
+        cout << endl;
       }
+    }
+  }
+
+  cout << adj.size() << " symbols" << endl;
+  for(auto symbol_pair : adj) {
+    cout << symbol_pair.first << " with size " << symbol_pair.second.size() << endl;
+    if(symbol_pair.second.size() == 2) {
+      cout << symbol_pair.first << " with ratio " << symbol_pair.second[0] << " * " << symbol_pair.second[1] << endl;
+      res += symbol_pair.second[0] * symbol_pair.second[1];
     }
   }
 
@@ -137,6 +160,8 @@ int main() {
   f.open("input.txt");
 
   part1(f);
+
+  cout << endl;
 
   f.clear();
   f.seekg(0);
